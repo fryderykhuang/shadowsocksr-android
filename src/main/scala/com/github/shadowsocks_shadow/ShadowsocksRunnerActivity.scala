25933d7cb1a3d5aa4f492dba98af45023f88b17d
Fryderyk Huang <fryderyk.huang@gmail.com>
2019-04-21 15:29:06 +0800

    Revert "disable VPN."
    This reverts commit 9668b47312cf3cfe1214b90053876ae331df7d66.

--- src/main/scala/com/github/shadowsocks_shadow/ShadowsocksRunnerActivity.scala
+++ src/main/scala/com/github/shadowsocks_shadow/ShadowsocksRunnerActivity.scala
@@ -69,13 +69,12 @@ class ShadowsocksRunnerActivity extends Activity with ServiceBoundContext {
       bgService.use(app.profileId)
       finish()
     } else {
-      //
-      //val intent = VpnService.prepare(ShadowsocksRunnerActivity.this)
-      //if (intent != null) {
-      //  startActivityForResult(intent, REQUEST_CONNECT)
-      //} else {
+      val intent = VpnService.prepare(ShadowsocksRunnerActivity.this)
+      if (intent != null) {
+        startActivityForResult(intent, REQUEST_CONNECT)
+      } else {
         onActivityResult(REQUEST_CONNECT, Activity.RESULT_OK, null)
-      //}
+      }
     }
   }
 
